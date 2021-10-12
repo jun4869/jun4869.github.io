9cbc76524d2619af7e70bdef8391b438799649b7
jun4869 <jun2194k@gmail.com>
2021-10-12 12:10:17 +0900

    pracitce ruby②

---
+++ ruby_project/method.rb
@@ -0,0 +1,21 @@
+def hello_world
+  puts 'Hello World!'
+end
+
+# hello_world
+
+def add(a, b)
+  a + b
+end
+
+# puts add(10, 1)
+
+def cry(animal)
+  if animal == 'cat'
+    'ニャー'
+  else
+    '???'
+  end
+end
+
+puts cry('cat')
