67d58e22cbfff34b6c14eeefe3e9ea3267984cd6
jun4869 <jun2194k@gmail.com>
2022-04-17 16:04:45 +0900

    portfolio_site html

--- 2021CG-main/Task2/index.js
+++ 2021CG-main/Task2/index.js
@@ -21,7 +21,7 @@ function init() {
   camera.position.set(0, 2, -13);
 
   // カメラコントローラーを作成
-  const controls = new THREE.OrbitControls(camera,document.body);
+  const controls = new THREE.OrbitControls(camera,canvasElement);
 
   // 平行光源1
   var directionalLight1 = new THREE.DirectionalLight(0xffffff);
@@ -74,140 +74,140 @@ function init() {
   mat5.map = texture5;
 
 // 平面を作成
-var planeGeometry = new THREE.PlaneGeometry(9,9,9,9);
-var plane = new THREE.Mesh(planeGeometry,mat);
-plane.rotation.set( -Math.PI/2, 0, 0 );
-plane.position.set(0,0,0);
+  var planeGeometry = new THREE.PlaneGeometry(9,9,9,9);
+  var plane = new THREE.Mesh(planeGeometry,mat);
+  plane.rotation.set( -Math.PI/2, 0, 0 );
+  plane.position.set(0,0,0);
 
-// 部品1を作成
-var sofa1_g = new THREE.BoxGeometry( 3, 2.4, 0.7);
-var sofa1 = new THREE.Mesh(sofa1_g, mat2);
-sofa1.position.set(0, 1.2,4);
+  // 部品1を作成
+  var sofa1_g = new THREE.BoxGeometry( 3, 2.4, 0.7);
+  var sofa1 = new THREE.Mesh(sofa1_g, mat2);
+  sofa1.position.set(0, 1.2,4);
 
-//部品2を作成
-var sofa2_g = new THREE.BoxGeometry( 3, 0.8, 1.8);
-var sofa2 = new THREE.Mesh( sofa2_g, mat2);
-sofa2.position.set(0,0.4,2.75);
+  //部品2を作成
+  var sofa2_g = new THREE.BoxGeometry( 3, 0.8, 1.8);
+  var sofa2 = new THREE.Mesh( sofa2_g, mat2);
+  sofa2.position.set(0,0.4,2.75);
 
-//部品3を作成
-var sofa3_g = new THREE.BoxGeometry( 0.7, 1.3, 2.5);
-var sofa3 = new THREE.Mesh( sofa3_g, mat2);
-sofa3.position.set(-1.85, 0.63, 3);
+  //部品3を作成
+  var sofa3_g = new THREE.BoxGeometry( 0.7, 1.3, 2.5);
+  var sofa3 = new THREE.Mesh( sofa3_g, mat2);
+  sofa3.position.set(-1.85, 0.63, 3);
 
-//部品4を作成
-var sofa4 = new THREE.Mesh( sofa3_g, mat2);
-sofa4.position.set(1.85, 0.63, 3);
+  //部品4を作成
+  var sofa4 = new THREE.Mesh( sofa3_g, mat2);
+  sofa4.position.set(1.85, 0.63, 3);
 
-//テーブル1を作成
-var table1_g = new THREE.BoxGeometry( 4.8, 0.2, 1.95 );
-var table1 = new THREE.Mesh( table1_g, mat4);
-table1.position.set(0,1.07,-0.58);
+  //テーブル1を作成
+  var table1_g = new THREE.BoxGeometry( 4.8, 0.2, 1.95 );
+  var table1 = new THREE.Mesh( table1_g, mat4);
+  table1.position.set(0,1.07,-0.58);
 
-//テーブル2を作成
-var table2_g = new THREE.BoxGeometry( 4, 0.1, 1.8 );
-var table2 = new THREE.Mesh( table2_g, mat4);
-table2.position.set(0,0.4,-0.58);
+  //テーブル2を作成
+  var table2_g = new THREE.BoxGeometry( 4, 0.1, 1.8 );
+  var table2 = new THREE.Mesh( table2_g, mat4);
+  table2.position.set(0,0.4,-0.58);
 
-//テーブル足の作成
-var table3_g = new THREE.CylinderGeometry( 0.13, 0.2, 1.0, 20, false);
-var table3 = new THREE.Mesh( table3_g, mat3);
-var table4 = new THREE.Mesh( table3_g, mat3);
-var table5 = new THREE.Mesh( table3_g, mat3);
-var table6 = new THREE.Mesh( table3_g, mat3);
-table3.position.set(1.93, 0.47,0.22);
-table4.position.set(1.93, 0.47,-1.35);
-table5.position.set(-1.93, 0.47,0.22);
-table6.position.set(-1.93, 0.47,-1.35);
+  //テーブル足の作成
+  var table3_g = new THREE.CylinderGeometry( 0.13, 0.2, 1.0, 20, false);
+  var table3 = new THREE.Mesh( table3_g, mat3);
+  var table4 = new THREE.Mesh( table3_g, mat3);
+  var table5 = new THREE.Mesh( table3_g, mat3);
+  var table6 = new THREE.Mesh( table3_g, mat3);
+  table3.position.set(1.93, 0.47,0.22);
+  table4.position.set(1.93, 0.47,-1.35);
+  table5.position.set(-1.93, 0.47,0.22);
+  table6.position.set(-1.93, 0.47,-1.35);
 
-//テレビ台
-var dai_g = new THREE.BoxGeometry( 6, 1.2, 1.8);
-var dai = new THREE.Mesh( dai_g, mat4);
-dai.position.set(0,0.55, -3.5);
+  //テレビ台
+  var dai_g = new THREE.BoxGeometry( 6, 1.2, 1.8);
+  var dai = new THREE.Mesh( dai_g, mat4);
+  dai.position.set(0,0.55, -3.5);
 
-//テレビ部品1
-var tv_g = new THREE.BoxGeometry( 1, 0.06, 0.8);
-var tv = new THREE.Mesh( tv_g, mat2);
-tv.position.set(0, 1.18, -3.5);
+  //テレビ部品1
+  var tv_g = new THREE.BoxGeometry( 1, 0.06, 0.8);
+  var tv = new THREE.Mesh( tv_g, mat2);
+  tv.position.set(0, 1.18, -3.5);
 
-//テレビ部品2
-var tv2_g = new THREE.BoxGeometry( 1, 0.08, 0.8);
-var tv2 = new THREE.Mesh( tv2_g, mat5);
-tv2.position.set(0, 1.18, -3.5);
+  //テレビ部品2
+  var tv2_g = new THREE.BoxGeometry( 1, 0.08, 0.8);
+  var tv2 = new THREE.Mesh( tv2_g, mat5);
+  tv2.position.set(0, 1.18, -3.5);
 
-//テレビ部品3
-var tv3_g = new THREE.BoxGeometry( 0.4, 1.4, 0.1);
-var tv3 = new THREE.Mesh( tv3_g, mat5);
-tv3.position.set(0, 1.18, -3.6);
+  //テレビ部品3
+  var tv3_g = new THREE.BoxGeometry( 0.4, 1.4, 0.1);
+  var tv3 = new THREE.Mesh( tv3_g, mat5);
+  tv3.position.set(0, 1.18, -3.6);
 
-//テレビ部品4
-var tv4_g = new THREE.BoxGeometry( 0.7, 0.5, 0.15);
-var tv4 = new THREE.Mesh( tv4_g, mat5);
-tv4.position.set(0, 1.85, -3.6);
+  //テレビ部品4
+  var tv4_g = new THREE.BoxGeometry( 0.7, 0.5, 0.15);
+  var tv4 = new THREE.Mesh( tv4_g, mat5);
+  tv4.position.set(0, 1.85, -3.6);
 
-//テレビ部品5
-var tv5_g = new THREE.BoxGeometry( 3, 1.4, 0.08);
-var tv5 = new THREE.Mesh( tv5_g, mat5);
-tv5.position.set(0, 2, -3.5);
+  //テレビ部品5
+  var tv5_g = new THREE.BoxGeometry( 3, 1.4, 0.08);
+  var tv5 = new THREE.Mesh( tv5_g, mat5);
+  tv5.position.set(0, 2, -3.5);
 
-//テレビ部品5
-var sp_g = new THREE.BoxGeometry( 0.7, 1.1, 1.3);
-var sp = new THREE.Mesh( sp_g, mat5);
-var sp2 = new THREE.Mesh( sp_g, mat5);
-sp.position.set(2.2, 1.7, -3.5);
-sp2.position.set(-2.2, 1.7, -3.5);
+  //テレビ部品5
+  var sp_g = new THREE.BoxGeometry( 0.7, 1.1, 1.3);
+  var sp = new THREE.Mesh( sp_g, mat5);
+  var sp2 = new THREE.Mesh( sp_g, mat5);
+  sp.position.set(2.2, 1.7, -3.5);
+  sp2.position.set(-2.2, 1.7, -3.5);
 
-var coneGeometry = new THREE.CylinderGeometry( 0, 0.1, 0.2, 32);
-var coneGeometry2 = new THREE.CylinderGeometry( 0, 0.16, 0.2, 32);
-var coneMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
-var cone = new THREE.Mesh(coneGeometry, coneMaterial);
-var cone3 = new THREE.Mesh(coneGeometry, coneMaterial);
-var cone2 = new THREE.Mesh(coneGeometry2, coneMaterial);
-var cone4 = new THREE.Mesh(coneGeometry2, coneMaterial);
-cone.rotation.set(-Math.PI/2, 0, 0 );
-cone2.rotation.set(-Math.PI/2, 0, 0 );
-cone3.rotation.set(-Math.PI/2, 0, 0 );
-cone4.rotation.set(-Math.PI/2, 0, 0 );
-cone.position.set(2.2, 1.84, -2.94);
-cone2.position.set(2.2, 1.4, -2.94);
-cone3.position.set(-2.2, 1.84, -2.94);
-cone4.position.set(-2.2, 1.4, -2.94);
+  var coneGeometry = new THREE.CylinderGeometry( 0, 0.1, 0.2, 32);
+  var coneGeometry2 = new THREE.CylinderGeometry( 0, 0.16, 0.2, 32);
+  var coneMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  var cone = new THREE.Mesh(coneGeometry, coneMaterial);
+  var cone3 = new THREE.Mesh(coneGeometry, coneMaterial);
+  var cone2 = new THREE.Mesh(coneGeometry2, coneMaterial);
+  var cone4 = new THREE.Mesh(coneGeometry2, coneMaterial);
+  cone.rotation.set(-Math.PI/2, 0, 0 );
+  cone2.rotation.set(-Math.PI/2, 0, 0 );
+  cone3.rotation.set(-Math.PI/2, 0, 0 );
+  cone4.rotation.set(-Math.PI/2, 0, 0 );
+  cone.position.set(2.2, 1.84, -2.94);
+  cone2.position.set(2.2, 1.4, -2.94);
+  cone3.position.set(-2.2, 1.84, -2.94);
+  cone4.position.set(-2.2, 1.4, -2.94);
 
-var textureLoader6 = new THREE.TextureLoader();  
-var texture6 = textureLoader6.load("img/plant.png");
-const material6 = new THREE.SpriteMaterial({ map: texture6,});
+  var textureLoader6 = new THREE.TextureLoader();  
+  var texture6 = textureLoader6.load("img/plant.png");
+  const material6 = new THREE.SpriteMaterial({ map: texture6,});
 
-const sprite = new THREE.Sprite(material6);
-sprite.scale.set(1, 5, 4)
-sprite.position.set(-3.7, 2.5, -3.5)
+  const sprite = new THREE.Sprite(material6);
+  sprite.scale.set(1, 5, 4)
+  sprite.position.set(-3.7, 2.5, -3.5)
 
-const liv = new THREE.Group();
- liv.add(sofa1); 
- liv.add(sofa2); 
- liv.add(sofa3); 
- liv.add(sofa4); 
- liv.add(table1); 
- liv.add(table2); 
- liv.add(table3); 
- liv.add(table4); 
- liv.add(table5); 
- liv.add(table6);
- liv.add(dai);
- liv.add(tv);
- liv.add(tv2);
- liv.add(tv3);
- liv.add(tv4);
- liv.add(tv5);
- liv.add(sp);
- liv.add(sp2);
- liv.add(cone);
- liv.add(cone2);
- liv.add(cone3);
- liv.add(cone4);
- liv.add(plane);
- liv.add(sprite);
- liv.position.set(0,0,0);
-scene.add(liv);
- //全体のオブジェクト
+  const liv = new THREE.Group();
+  liv.add(sofa1); 
+  liv.add(sofa2); 
+  liv.add(sofa3); 
+  liv.add(sofa4); 
+  liv.add(table1); 
+  liv.add(table2); 
+  liv.add(table3); 
+  liv.add(table4); 
+  liv.add(table5); 
+  liv.add(table6);
+  liv.add(dai);
+  liv.add(tv);
+  liv.add(tv2);
+  liv.add(tv3);
+  liv.add(tv4);
+  liv.add(tv5);
+  liv.add(sp);
+  liv.add(sp2);
+  liv.add(cone);
+  liv.add(cone2);
+  liv.add(cone3);
+  liv.add(cone4);
+  liv.add(plane);
+  liv.add(sprite);
+  liv.position.set(0,0,0);
+  scene.add(liv);
+  //全体のオブジェクト
 
 
 
